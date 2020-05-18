ee6bff41f6af21f23d9c8c7087d339ff53e29a22
Hugo Solar <hugo.solar@gmail.com>
2020-05-18 11:14:03 -0400

    custom endpoint and global navigation support

---
+++ inc/filters.php
@@ -0,0 +1,18 @@
+<?php
+class CC_Org_Filters {
+  public static function get_main_menu() {
+    $menu_locations = get_nav_menu_locations();
+    $main_menu = wp_get_nav_menu_object($menu_locations['main-navigation']);
+    if ( !empty( $main_menu ) ) {
+      return wp_get_nav_menu_items($main_menu);
+    }
+  }
+  public static function set_custom_menu_endpoint() {
+    register_rest_route( 'ccglobal', '/menu', array(
+      'methods' => 'GET',
+      'callback' => array('CC_Org_Filters','get_main_menu'),
+	  ));
+  }
+}
+
+add_action( 'rest_api_init', array( 'CC_Org_Filters', 'set_custom_menu_endpoint') );
