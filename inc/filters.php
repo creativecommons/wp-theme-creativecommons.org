2fae29efc8238ca5fe53d784b48ae6365ec6daaa
Hugo Solar <hugo.solar@gmail.com>
2020-06-18 11:40:58 -0400

    add custom endpoints to return header & footer menu elements

--- inc/filters.php
+++ inc/filters.php
@@ -1,6 +1,6 @@
 <?php
 class CC_Org_Filters {
-  public static function get_main_menu() {
+  public static function get_global_menu() {
     if ( false === ( $global_menu_items = get_transient( 'global_menu_items' ) ) ) {
       $menu_locations = get_nav_menu_locations();
       $global_menu = wp_get_nav_menu_object($menu_locations['global-menu']);
@@ -11,10 +11,44 @@ class CC_Org_Filters {
     }
     return $global_menu_items;
   }
+  public static function get_header_menu() {
+    if ( false === ( $global_menu_items = get_transient( 'header_menu_items' ) ) ) {
+      $menu_locations = get_nav_menu_locations();
+      header_menu = wp_get_nav_menu_object($menu_locations['main-navigation']);
+      if ( !empty( header_menu ) ) {
+        header_menu_items = wp_get_nav_menu_items(header_menu);
+        set_transient( 'header_menu_items', header_menu_items, 1 * HOUR_IN_SECONDS );
+      }
+    }
+    return $global_menu_items;
+  }
+  public static function get_footer_menu() {
+    if ( false === ( $global_menu_items = get_transient( 'footer_menu_items' ) ) ) {
+      $menu_locations = get_nav_menu_locations();
+      $footer_menu = wp_get_nav_menu_object($menu_locations['footer-navigation']);
+      if ( !empty( $footer_menu ) ) {
+        $footer_menu_items = wp_get_nav_menu_items($footer_menu);
+        set_transient( 'footer_menu_items', $footer_menu_items, 1 * HOUR_IN_SECONDS );
+      }
+    }
+    return $global_menu_items;
+  }
   public static function set_custom_menu_endpoint() {
     register_rest_route( 'ccglobal', '/menu', array(
       'methods' => 'GET',
       'callback' => array('CC_Org_Filters','get_main_menu'),
+	  ));
+  }
+  public static function set_header_menu_endpoint() {
+    register_rest_route( 'ccnavigation-header', '/menu', array(
+      'methods' => 'GET',
+      'callback' => array('CC_Org_Filters','get_header_menu'),
+	  ));
+  }
+  public static function set_footer_menu_endpoint() {
+    register_rest_route( 'ccnavigation-footer', '/menu', array(
+      'methods' => 'GET',
+      'callback' => array('CC_Org_Filters','get_footer_menu'),
 	  ));
   }
   /**
@@ -25,6 +59,8 @@ class CC_Org_Filters {
    */
   function remove_global_menu_transient($nav_menu_selected_id) {
     delete_transient( 'global_menu_items' );
+    delete_transient( 'header_menu_items' );
+    delete_transient( 'footer_menu_items' );
   }
 }
 
