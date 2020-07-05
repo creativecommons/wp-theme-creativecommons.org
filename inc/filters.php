b88030b7fa2e5db002235b38c65b532da5579b7d
Hugo Solar <hugo.solar@gmail.com>
2020-07-05 17:02:21 -0400

    add endpoint for collecting paths of enqueued styles and scripts

--- inc/filters.php
+++ inc/filters.php
@@ -33,6 +33,56 @@ class CC_Org_Filters {
     }
     return $footer_menu_items;
   }
+  public static function transient_queued_scripts() {
+    delete_transient('cc_enqueued_scripts');
+    if ( false === ( $script_list = get_transient( 'cc_enqueued_scripts' ) ) ) {
+      global $wp_scripts;
+      $script_list = array();
+      foreach ( $wp_scripts->queue as $handler ) {
+        if ($handler == 'jquery') {
+          foreach ( $wp_scripts->registered[$handler]->deps as $script_dep_handler ) {
+            $src = $wp_scripts->registered[$script_dep_handler]->src;
+            if ( !empty( $src ) ) {
+              $script_list[$script_dep_handler] = self::maybe_correct_path( $src );
+            }
+          }
+        } else {
+          $src = $wp_scripts->registered[$handler]->src;
+            $script_list[$handler] = self::maybe_correct_path( $src );
+        }
+      }
+      set_transient( 'cc_enqueued_scripts', $script_list, 1 * HOUR_IN_SECONDS );
+    }
+  }
+  public static function transient_queued_styles() {
+    delete_transient('cc_enqueued_styles');
+    if ( false === ( $style_list = get_transient( 'cc_enqueued_styles' ) ) ) {
+      global $wp_styles;
+      $styles_list = array();
+      foreach ( $wp_styles->queue as $handler ) {
+        $src = $wp_styles->registered[$handler]->src;
+        $styles_list[$handler] = self::maybe_correct_path( $src );
+      }
+      set_transient( 'cc_enqueued_styles', $styles_list, 1 * HOUR_IN_SECONDS );
+    }
+  }
+  static public function maybe_correct_path( $url ) {
+    $parse_url = wp_parse_url( $url );
+    if ( empty( $parse_url['host'] ) ) {
+      $return_url = get_bloginfo( 'url' ) . $url;
+    } else {
+      $return_url = $url;
+    }
+    return $return_url;
+  }
+  public static function get_queued_scripts() {
+    $script_list = get_transient( 'cc_enqueued_scripts' );
+    return $script_list;
+  }
+  public static function get_queued_styles() {
+    $styles_list = get_transient( 'cc_enqueued_styles' );
+    return $styles_list;
+  }
   public static function set_global_menu_endpoint() {
     register_rest_route( 'ccglobal', '/menu', array(
       'methods' => 'GET',
@@ -49,6 +99,18 @@ class CC_Org_Filters {
     register_rest_route( 'ccnavigation-footer', '/menu', array(
       'methods' => 'GET',
       'callback' => array('CC_Org_Filters','get_footer_menu'),
+	  ));
+  }
+  public static function set_queued_scripts_endpoint() {
+    register_rest_route( 'cc-wpscripts', '/get', array(
+      'methods' => 'GET',
+      'callback' => array('CC_Org_Filters','get_queued_scripts'),
+	  ));
+  }
+  public static function set_queued_styles_endpoint() {
+    register_rest_route( 'cc-wpstyles', '/get', array(
+      'methods' => 'GET',
+      'callback' => array('CC_Org_Filters','get_queued_styles'),
 	  ));
   }
   /**
@@ -68,3 +130,7 @@ add_action( 'wp_update_nav_menu', array( 'CC_Org_Filters', 'remove_global_menu_t
 add_action( 'rest_api_init', array( 'CC_Org_Filters', 'set_global_menu_endpoint') );
 add_action( 'rest_api_init', array( 'CC_Org_Filters', 'set_header_menu_endpoint') );
 add_action( 'rest_api_init', array( 'CC_Org_Filters', 'set_footer_menu_endpoint') );
+add_action( 'rest_api_init', array( 'CC_Org_Filters', 'set_queued_scripts_endpoint') );
+add_action( 'rest_api_init', array( 'CC_Org_Filters', 'set_queued_styles_endpoint') );
+add_action( 'wp_print_scripts', array( 'CC_Org_Filters', 'transient_queued_scripts'), 100 );
+add_action( 'wp_print_styles', array( 'CC_Org_Filters', 'transient_queued_styles'), 100 );
